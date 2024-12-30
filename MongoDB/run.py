0359d4a3732597a580e23f3ed3f41a1110fb1894
IanaNeri22 <iananeri@outlook.com>
2024-12-30 01:04:36 -0300

    feat: swagger e requirements.txt

--- MongoDB/run.py
+++ MongoDB/run.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from models.blueprints.cliente_blueprints import clientes_bp
 from config import Config
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 app = Flask(__name__)
+
+SWAGGER_URL="/swagger"
+API_URL="/static/swagger.json"
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Access API'
+    }
+)
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 app.register_blueprint(clientes_bp, url_prefix="/cliente")
 
